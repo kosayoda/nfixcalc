1079be28bbed9abe62cac6dc39e7003b284086e7
kosayoda <kieransiek@gmail.com>
2019-05-08 11:49:32 +0800

    Add calculation functionality

--- nfixcalc/buffer.py
+++ nfixcalc/buffer.py
@@ -4,46 +4,93 @@ from nfixcalc.calculator import OPERATORS
 
 
 class Buffer:
+    """
+    Buffer class to store and handle tokens in the current equation
+
+    Attributes:
+        equation: List of tokens
+            Example: ["12.3", "+", "-6"]
+        temp: List of characters in an incomplete token
+            Example: ["1", "5", ".", "2", "6"]
+    """
     def __init__(self):
-        self.equation: List[str] = []
-        self.temp_val: List[str] = []
+        self._equation: List[str] = []
+        self.temp: List[str] = []
 
     def add(self, token: str):
-        print(f"{token} pressed!")
+        """
+        Adds a token to the current equation
+
+        Args:
+            token: The token to add to the equation
+        """
+        # Operators mean that the current number is finished, therefore we can
+        # flush the buffer. If not, the next token may still be the subsequent
+        # character in a number.
         if token in OPERATORS:
             self.flush()
-            self.equation.append(token)
+            self._equation.append(token)
         else:
-            self.temp_val.append(token)
+            self.temp.append(token)
 
     def delete(self):
-        if self.temp_val:
-            self.temp_val.pop()
-        elif self.equation:
-            val = self.equation.pop()
+        """
+        Removes the last character in the current equation
+        """
+        # Characters in `temp` can simply be removed.
+        # Since `equation` is a list of tokens, we need to get the last entered
+        # token to remove the last character.
+        if self.temp:
+            self.temp.pop()
+        elif self._equation:
+            val = self._equation.pop()
             if len(val) > 1:
-                self.temp_val.append(val[:-1])
+                self.temp.append(val[:-1])
 
     def flush(self):
-        if self.temp_val:
-            self.equation.append("".join(self.temp_val))
-            self.temp_val.clear()
+        """
+        Joins the current token in `temp` and adds it to the equation
+        """
+        if self.temp:
+            self._equation.append("".join(self.temp))
+            self.temp.clear()
 
     def clear(self):
-        self.equation.clear()
-        self.temp_val.clear()
+        """
+        Clears all buffers in the system
+        """
+        self._equation.clear()
+        self.temp.clear()
 
-    def invert_digit(self):
-        if self.temp_val:
-            if self.temp_val[0] == "-":
-                self.temp_val.pop()
+    def invert_sign(self):
+        """
+        Change the sign of the last entered number from positive to negative
+        and vice versa
+        """
+        if self.temp:
+            if self.temp[0] == "-":
+                self.temp.pop(0)
             else:
-                self.temp_val.insert(0, "-")
-        elif self.equation:
-            val = self.equation.pop()
+                self.temp.insert(0, "-")
+        elif self._equation:
+            val = self._equation.pop()
             if val not in OPERATORS:
                 if val.startswith("-"):
                     val = val[1:]
                 else:
                     val = f"-{val}"
-            self.equation.append(val)
+            self._equation.append(val)
+
+    @property
+    def equation(self):
+        self.flush()
+        return self._equation
+
+    @property
+    def equation_label(self):
+        display = ""
+        if self._equation:
+            display += f"{' '.join(self._equation)} "
+        if self.temp:
+            display += f"{''.join(self.temp)}"
+        return display
--- nfixcalc/gui.py
+++ nfixcalc/gui.py
@@ -1,21 +1,45 @@
-import functools
 import sys
+from enum import Enum
+from functools import partial
+from itertools import cycle
 
 import qdarkstyle
 from PySide2.QtWidgets import QApplication, QDesktopWidget, QMainWindow
 
+from nfixcalc import calculator
 from nfixcalc import ui
 from nfixcalc.buffer import Buffer
+
+
+class Mode(Enum):
+    INFIX = 0
+    PREFIX = 1
+    POSTFIX = 2
+
+    def __str__(self):
+        return self.name.title()
+
+    @property
+    def key_text(self):
+        if self is Mode.INFIX:
+            return "(", ")"
+        else:
+            return "Enter ↑", "Check Equation"
 
 
 class MainApplication(QMainWindow):
     def __init__(self):
         super().__init__()
+
         self.ui = ui.MainWindow()
         self.ui.setupUi(self)
         self.center()
 
         self.buffer = Buffer()
+        self.result = 0
+
+        self.modes = cycle(Mode)
+        self.cycle_mode()
 
         self.bind_buttons()
 
@@ -29,11 +53,73 @@ class MainApplication(QMainWindow):
         self.move(frame.topLeft())
 
     def bind_buttons(self):
+        """
+        Binds buttons to their respective functions
+        """
+        # Numbers, period and operator keys
         for key in self.ui.tokens.buttons():
             key_name = key.text()
-            key.clicked.connect(
-                functools.partial(self.buffer.add, key_name)
+            key.clicked.connect(partial(self.buffer.add, key_name))
+
+        # Special function keys
+        self.ui.key_sign.clicked.connect(self.buffer.invert_sign)
+        self.ui.key_ac.clicked.connect(self.buffer.clear)
+        self.ui.key_del.clicked.connect(self.buffer.delete)
+        self.ui.key_mode.clicked.connect(self.cycle_mode)
+        self.ui.key_solve.clicked.connect(
+            lambda: self.calculate(self.mode, self.buffer.equation)
+        )
+        self.ui.key_extra1.clicked.connect(lambda: self.ex_clicked(0))
+        self.ui.key_extra2.clicked.connect(lambda: self.ex_clicked(1))
+
+        # Map each key to the label update function
+        layout = self.ui.gridlayout
+        for key in (layout.itemAt(i).widget() for i in range(layout.count())):
+            key.clicked.connect(self.update)
+
+    def update(self):
+        self.ui.info_label.setText(self.buffer.equation_label)
+        self.ui.screen.display(self.result)
+
+    def cycle_mode(self):
+        self.mode = next(self.modes)
+
+        text_1, text_2 = self.mode.key_text
+        self.ui.key_extra1.setText(text_1)
+        self.ui.key_extra2.setText(text_2)
+
+        self.ui.statusbar.showMessage(f"Mode: {self.mode}")
+
+    def ex_clicked(self, key):
+        if self.mode is Mode.INFIX:
+            self.buffer.add(self.mode.text[key])
+        else:
+            func = {
+                0: self.buffer.flush,
+                1: self.check_equation,
+            }
+            func[key]()
+
+    def calculate(self, mode, equation):
+        mode_function = {
+            Mode.PREFIX: calculator.calc_prefix,
+            Mode.INFIX: calculator.calc_infix,
+            Mode.POSTFIX: calculator.calc_postfix,
+        }
+        try:
+            self.result = mode_function[mode](equation)
+        except IndexError:
+            self.ui.statusbar.showMessage(
+                f"Error: Invalid equation for mode {self.mode}"
             )
+        except ZeroDivisionError:
+            self.ui.statusbar.showMessage(
+                f"Error: Division by zero"
+            )
+        return self.result
+
+    def check_equation(self):
+        pass
 
 
 def main():
